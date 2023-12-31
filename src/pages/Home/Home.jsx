815d26c7c5641bc5b9a2d528c2d1b82edf880297
tanviralways <parthotanvir1234@gmail.com>
2023-12-31 09:11:31 +0600

    complete banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,18 @@
+import svg from "../../assets/38814296_503566823401613_8014003606732668928_n.jpg";
+import "./home.css";
 const Home = () => {
   return (
     <div>
-      <h2>homeeeeeeee</h2>
+      <div className="banner">
+        <div>
+          <p>We have <span>200,000</span> great job offers you deserve!</p>
+          <h1>Largets Job Site In <br /> The World</h1>
+          <button>Explore Now</button>
+        </div>
+        <div>
+          <img src={svg} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
